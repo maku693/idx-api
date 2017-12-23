595cfd42041f1b56991df96b2b66c9404b6167b5
Shota HAMADA <maku693@outlook.com>
2017-12-23 14:52:26 +0900

    Make the server address configurable via the command line argument

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var addr = flag.String("addr", ":8080", "the address of the server")
+
 func main() {
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, golang")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
